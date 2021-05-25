db8dc3b589dbadddb7c82a5069ce78bca3403189
Hariel <hariel@hotmail.com>
2021-05-25 17:38:45 -0300

    25/05

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 //Conexão com BD;
 const pool = new Pool({
-    connectionString: "postgres://oorvnhwgsbnimx:97894f68450f0f062be9e48b11edcb9c80fc0957f2bbc6a9c84b7296071f0bdb@ec2-54-197-100-79.compute-1.amazonaws.com:5432/dd4l04u3h53877?sslmode=require",
+    connectionString: process.env.DATABASE_URL
   })
 
   pool.on('connect', () => {
--- src/config/databaseSeq.js
+++ src/config/databaseSeq.js
@@ -2,17 +2,11 @@ require('dotenv').config()
 
 //Sequelize
 module.exports = {
-    'username': "oorvnhwgsbnimx",
-    'password': "97894f68450f0f062be9e48b11edcb9c80fc0957f2bbc6a9c84b7296071f0bdb",
-    'database': "dd4l04u3h53877",
-    'host': "ec2-54-197-100-79.compute-1.amazonaws.com",
+    'username': global.process.env.DATABASE_USERNAME,
+    'password': global.process.env.DATABASE_PASSWORD,
+    'database': global.process.env.DATABASE_DATABASE,
+    'host': global.process.env.DATABASE_HOST,
     dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-          require: true, // This will help you. But you will see nwe error
-          rejectUnauthorized: false // This line will fix new error
-        }
-    },
     define: {
         timestamp: true, // created_at, update_at
         underscored: true, //formato das tabelas snake case
