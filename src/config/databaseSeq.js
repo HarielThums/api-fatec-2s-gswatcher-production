cd5cf89df7d6bd6be130297df870aba614d1be87
Hariel <hariel@hotmail.com>
2021-05-25 13:26:10 -0300

    25/05

--- src/config/databaseSeq.js
+++ src/config/databaseSeq.js
@@ -7,6 +7,12 @@ module.exports = {
     'database': "dd4l04u3h53877",
     'host': "ec2-54-197-100-79.compute-1.amazonaws.com",
     dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+          require: true, // This will help you. But you will see nwe error
+          rejectUnauthorized: false // This line will fix new error
+        }
+    },
     define: {
         timestamp: true, // created_at, update_at
         underscored: true, //formato das tabelas snake case
