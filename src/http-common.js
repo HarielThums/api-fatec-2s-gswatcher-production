04ef2adf8cd257456b62c347420db3614cfc5008
Hariel <hariel@hotmail.com>
2021-06-06 20:55:16 -0300

    06/06

--- src/http-common.js
+++ src/http-common.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api-gswatcher.herokuapp.com/api",
   headers: {
+    'Authorization': 'Bearer ' + localStorage.getItem('@gswatcher:token'),
     "Content-type": "application/json"
   }
 });
@@ -10,13 +11,15 @@ export default axios.create({
 const http = axios.create({
   baseURL: "https://api-gswatcher.herokuapp.com/api",
   headers: {
+    'Authorization': 'Bearer ' + localStorage.getItem('@gswatcher:token'),
     "Content-type": "application/json"
   }
 });
 
 const auth = axios.create({
   baseURL: "https://api-gswatcher.herokuapp.com/auth",
-  headers: {
+  headers: {    
+    'Authorization': 'Bearer ' + localStorage.getItem('@gswatcher:token'),
     "Content-type": "application/json"
   }
 });
